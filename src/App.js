b707ccd1b5d2cc74724b79a8acf8061221bb3c4c
alvaro-andrade <alvaro.andrade@salesforce.com>
2019-09-22 00:15:04 -0300

    Added risk table

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.scss";
 // Components
 import { Header } from "./components/Header";
 import RiskSelector from "./components/RiskSelector";
+import RiskTable from "./components/RiskTable";
 
 function App() {
   return (
     <div className="App">
       <Header title="Financial Advisor" />
       <RiskSelector />
+      <RiskTable />
     </div>
   );
 }
