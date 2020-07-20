78d8ab978bd9d7c7435fc199a4d487ec8c544086
Alvaro Andrade <varito1@gmail.com>
2020-07-20 02:10:08 -0300

    Updated packages and some css rules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./App.scss";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './App.scss';
 
 // Components
-import Header from "./components/Header";
-import RiskSelector from "./components/RiskSelector";
-import RiskCalculator from "./components/RiskCalculator";
+import Header from './components/Header';
+import RiskSelector from './components/RiskSelector';
+import RiskCalculator from './components/RiskCalculator';
 
 const App = () => (
   <Router>
